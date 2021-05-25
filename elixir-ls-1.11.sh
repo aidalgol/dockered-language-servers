5169c6365728bce27e32ec9400f030627ab70095
Aidan Gauland <aidalgol+git@fastmail.net>
2021-05-25 22:36:13 +1200

    Make it possible to run elixir-ls under docker transparently
    You can now use this with plain lsp.el

---
+++ elixir-ls-1.11.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+exec docker run \
+     --interactive \
+     --user="$(id -u):$(id -g)" \
+     --env="MIX_HOME=$HOME/.mix" \
+     --mount="type=bind,source=$HOME,destination=$HOME" \
+     elixir-ls:1.11 "$@"
